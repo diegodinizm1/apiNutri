0e3ae4da41a9c07acd92e4e5017b2d30399f7bf1
diegodinizm1 <diegocavalcantepb1@hotmail.com>
2025-06-02 16:28:24 -0300

    first commit

---
+++ src/main/java/com/diego/apinutri/ApiNutriApplication.java
@@ -0,0 +1,13 @@
+package com.diego.apinutri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiNutriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiNutriApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/diego/apinutri/ApiNutriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego.apinutri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiNutriApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
